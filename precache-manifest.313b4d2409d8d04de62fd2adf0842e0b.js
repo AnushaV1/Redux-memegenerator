876a616103425908cdb8bac3b56ee21fd813a360
AnushaV1 <anush_chander@yahoo.com>
2020-12-23 21:04:47 -0800

    Updates

---
+++ precache-manifest.313b4d2409d8d04de62fd2adf0842e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fd48eb926ebf45600d3cdb4a107598",
+    "url": "/Redux-memegenerator/index.html"
+  },
+  {
+    "revision": "dc5f540f4c5212bdb293",
+    "url": "/Redux-memegenerator/static/css/main.e0255488.chunk.css"
+  },
+  {
+    "revision": "f9593db98f166c008891",
+    "url": "/Redux-memegenerator/static/js/2.1eec2105.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Redux-memegenerator/static/js/2.1eec2105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5f540f4c5212bdb293",
+    "url": "/Redux-memegenerator/static/js/main.9d53c9cd.chunk.js"
+  },
+  {
+    "revision": "a6d489a8b31e2a6cb1bb",
+    "url": "/Redux-memegenerator/static/js/runtime-main.cb1e97ce.js"
+  }
+]);
